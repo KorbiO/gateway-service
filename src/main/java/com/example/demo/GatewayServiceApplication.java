0e16d1f45a2eb1533bbbdbe6259da33d87e960de
omarkorbi <omarkorbi@outlook.com>
2020-12-23 15:57:26 +0100

    test120

--- src/main/java/com/example/demo/GatewayServiceApplication.java
+++ src/main/java/com/example/demo/GatewayServiceApplication.java
@@ -13,6 +13,7 @@ public class GatewayServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayServiceApplication.class, args);
+		System.out.println("omar");
 	}
 
 }
